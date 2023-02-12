350d890578b4f10ddd4213d44a17f20f0b21268d
Niexiawei <1135215907@qq.com>
2023-02-12 16:54:46 +0800

    去除对gin的依赖

--- authentication.go
+++ authentication.go
@@ -3,7 +3,6 @@ package authentication
 import (
 	"errors"
 	"github.com/Niexiawei/golang-utils/httpresponse"
-	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v4"
 	"sync"
 	"time"
@@ -173,46 +172,58 @@ func (j *Jwt) GetUserByToken(t string) (AuthenticationUserModel, error) {
 	return user, nil
 }
 
-func AuthenticationMiddleware(j *Jwt) func(c *gin.Context) {
-	return func(c *gin.Context) {
+type ginContext interface {
+	GetHeader(key string) string
+	GetQuery(key string) (string, bool)
+	Abort()
+	Next()
+	Set(key string, value any)
+	JSON(code int, obj any)
+}
 
-		var (
-			token string
-			resp  = httpresponse.NewResponse(c, 0, "")
-		)
-
-		t1 := c.GetHeader("token")
-		t2, _ := c.GetQuery("token")
-
-		if t1 == "" && t2 == "" {
-			resp.WithMessage(TokenVerifyError.Error()).WithCode(401).ResultOk()
-			c.Abort()
-			return
-		}
-
-		if t2 != "" {
-			token = t2
-		}
-
-		if t1 != "" {
-			token = t1
-		}
-
-		auth, err := j.GetUserByToken(token)
-
-		if err != nil {
-			if errors.Is(err, TokenVerifyError) {
-				resp.WithMessage(TokenVerifyError.Error()).WithCode(401).ResultOk()
-			} else if errors.Is(err, TokenVerifyExpireError) {
-				resp.WithMessage(TokenVerifyExpireError.Error()).WithCode(403).ResultOk()
-			} else {
-				resp.WithMessage(TokenVerifyError.Error()).WithCode(401).ResultOk()
-			}
-			c.Abort()
-			return
-		}
-
-		c.Set(GetAuthKey, auth)
-		c.Next()
+func AuthenticationMiddleware[T ginContext](j *Jwt) func(T) {
+	return func(c T) {
+		Authentication(j, c)
 	}
+}
+
+func Authentication(j *Jwt, c ginContext) {
+	var (
+		token string
+		resp  = httpresponse.NewResponse(c, 0, "")
+	)
+
+	t1 := c.GetHeader("token")
+	t2, _ := c.GetQuery("token")
+
+	if t1 == "" && t2 == "" {
+		resp.WithMessage(TokenVerifyError.Error()).WithCode(401).ResultOk()
+		c.Abort()
+		return
+	}
+
+	if t2 != "" {
+		token = t2
+	}
+
+	if t1 != "" {
+		token = t1
+	}
+
+	auth, err := j.GetUserByToken(token)
+
+	if err != nil {
+		if errors.Is(err, TokenVerifyError) {
+			resp.WithMessage(TokenVerifyError.Error()).WithCode(401).ResultOk()
+		} else if errors.Is(err, TokenVerifyExpireError) {
+			resp.WithMessage(TokenVerifyExpireError.Error()).WithCode(403).ResultOk()
+		} else {
+			resp.WithMessage(TokenVerifyError.Error()).WithCode(401).ResultOk()
+		}
+		c.Abort()
+		return
+	}
+
+	c.Set(GetAuthKey, auth)
+	c.Next()
 }
